4ce623b64c185f3298db472252fb15f83916e5a1
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2023-02-20 11:03:42 +0000

    Audio

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource[] audios_Music;
+    public AudioSource[] audios_SFX;
+    public Toggle musictoggle;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public GameObject music;
+
+    public void SetVolumeMusic(float volume)
     {
-        
+        musicSlider.value = volume;
+
+        foreach (AudioSource audio in audios_Music)
+        {
+            audio.volume = musicSlider.value;
+        }
+    }
+    public void SetVolumeSFX(float volume)
+    { 
+        sfxSlider.value = volume;
+
+        foreach (AudioSource audio in audios_SFX)
+       {
+            audio.volume = sfxSlider.value;
+        }
+    }
+    public void ToggleMusic(int logic)
+    {
+        if(musictoggle.isOn == true)
+        {
+            logic = 1;
+            musicSlider.interactable = true;
+            musicSlider.value = 0.5f;
+        }
+        else if(musictoggle.isOn == false)
+        {
+            logic = 0;
+            musicSlider.interactable = false;
+            musicSlider.value = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
